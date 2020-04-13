6cf4f40117f2754c0f0b427b505895520e209223
Ernest Muli <ernestkmuli@gmail.com>
2020-04-13 23:02:59 +0300

    Initial challenge 1 commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,36 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  const {
+    region: { name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation },
+    periodType,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  } = data;
+
+  let { timeToElapse } = data;
+  if (periodType === 'months') {
+    timeToElapse = Math.trunc(timeToElapse * 30);
+  } else if (periodType === ' weeks') {
+    timeToElapse = Math.trunc(timeToElapse * 7);
+  } else {
+    timeToElapse = Math.trunc(timeToElapse * 1);
+  }
+  const impactCurrentInfected = reportedCases * 10;
+  const severeImpactCurrentInfected = reportedCases * 50;
+  const squarefactor = 1024;
+
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: impactCurrentInfected,
+      infectionsByRequestedTime: impactCurrentInfected * squarefactor
+    },
+    severeImpact: {
+      currentlyInfected: severeImpactCurrentInfected,
+      infectionsByRequestedTime: severeImpactCurrentInfected * squarefactor
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
